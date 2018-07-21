653b4cbe289fcf70434e76ed18f2abed2450f700
itaycsguy <itgu1010@gmail.com>
2018-07-22 01:22:57 +0300

    Handle CUSTOM_OPEN_POSE not exist case

--- const_and_packages.py
+++ const_and_packages.py
@@ -1,6 +1,11 @@
 import sys,numpy as np,math,os
+open_pose_main_dir = None
 try:
     open_pose_main_dir = os.environ['CUSTOM_OPEN_POSE']
+    print(open_pose_main_dir)
+    if not open_pose_main_dir:
+        print("Cannot executing without 'CUSTOM_OPEN_POSE' environment variable definition.")
+        sys.exit(-1)
     sys.path.append(open_pose_main_dir)
 except:
     print("Cannot executing without 'CUSTOM_OPEN_POSE' environment variable definition.")
